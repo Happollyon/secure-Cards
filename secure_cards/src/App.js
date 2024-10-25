e488b0915c69ae9b390bdc8e19299d7676bed2dc
Happollyon <36013973+Happollyon@users.noreply.github.com>
2024-10-25 10:19:48 +0000

    first commit

---
+++ secure_cards/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="sideNav">
+         Sercurity+ Cards
+      </div>
+      <img src={} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
